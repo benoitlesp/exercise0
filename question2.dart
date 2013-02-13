3a61011b5e7f2a3140f80daf5eb3bde6d2bf01a3
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-02-13 17:06:05 -0500

    dr corrections

--- question2.dart
+++ question2.dart
@@ -2,7 +2,8 @@
 main()  {
   var temps = 6.892;
   var distance = 19.7;
-  var vitesse = temps * distance;
+  //var vitesse = temps * distance;
+  var vitesse = distance/temps;
   print(vitesse);
 }
 
--- question4.dart
+++ question4.dart
@@ -1,31 +1,33 @@
 main() {
   
-  var Fibon = new List();
-  Fibon.add(0);
-  Fibon.add(1);
+  //var Fibon = new List();
+  var fibon = new List();
+  fibon.add(0);
+  fibon.add(1);
   var total = 0;
   /* je comprends mal l’obligation d’avoir le mot elementAt , j’imagine qu’il
   *est réservé pour inscrire un élement, 
   */
-  total = Fibon.elementAt(0) + Fibon.elementAt(1);
-  Fibon.add(total);
-  total = Fibon.elementAt(1) + Fibon.elementAt(2);
-  Fibon.add(total);
-  total = Fibon.elementAt(2) + Fibon.elementAt(3);
-  Fibon.add(total);
-  total = Fibon.elementAt(3) + Fibon.elementAt(4);
-  Fibon.add(total);
-  total = Fibon.elementAt(4) + Fibon.elementAt(5);
-  Fibon.add(total);
-  total = Fibon.elementAt(5) + Fibon.elementAt(6);
-  Fibon.add(total);
-  total = Fibon.elementAt(6) + Fibon.elementAt(7);
-  Fibon.add(total);
-  total = Fibon.elementAt(7) + Fibon.elementAt(8);
-  Fibon.add(total);
-  total = Fibon.elementAt(8) + Fibon.elementAt(9);
-  Fibon.add(total);
-  print(Fibon);
+  //total = Fibon.elementAt(0) + Fibon.elementAt(1);
+  total = fibon[0] + fibon[1];
+  fibon.add(total);
+  total = fibon.elementAt(1) + fibon.elementAt(2);
+  fibon.add(total);
+  total = fibon.elementAt(2) + fibon.elementAt(3);
+  fibon.add(total);
+  total = fibon.elementAt(3) + fibon.elementAt(4);
+  fibon.add(total);
+  total = fibon.elementAt(4) + fibon.elementAt(5);
+  fibon.add(total);
+  total = fibon.elementAt(5) + fibon.elementAt(6);
+  fibon.add(total);
+  total = fibon.elementAt(6) + fibon.elementAt(7);
+  fibon.add(total);
+  total = fibon.elementAt(7) + fibon.elementAt(8);
+  fibon.add(total);
+  total = fibon.elementAt(8) + fibon.elementAt(9);
+  fibon.add(total);
+  print(fibon);
 
 }
 
